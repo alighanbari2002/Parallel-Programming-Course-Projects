0ea89f43f958dc7bd38f3d9ab760be674d1142b2
Ali Ghanbari <ali4.ghanbari7@gmail.com>
2023-11-03 20:57:36 +0330

    Refactor main.cpp

--- CA2/Q1/main.cpp
+++ CA2/Q1/main.cpp
@@ -3,6 +3,7 @@
 // #include <ipp.h>
 #include <sys/time.h>
 #include <time.h>
+#include <float.h>
 #include <math.h>
 #ifdef 		_WIN32
 #include <intrin.h>
@@ -19,7 +20,7 @@ void generate_random_array(float* arr, size_t size) {
 	
 	for (size_t i = 0; i < size; i++) {
 		srand(time(NULL));
-		float random = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX));
+		float random =  ((float)rand()) / (float)RAND_MAX;
 		arr[i] = random * range + min;
 	}
 }
@@ -94,66 +95,78 @@ long int find_min_serial(float* array, size_t size) {
 
 	gettimeofday(&end, NULL);
 
+    // Calculate the execution time in microseconds
 	long int execution_time = (((end.tv_sec - start.tv_sec) * 1000000) + end.tv_usec) - (start.tv_usec);	
 	printf("Serial Method:\n");
-	printf("\tMin value: %f\n", min_element);
-	printf("\tMin index: %d\n", min_index);
-	printf("\tExecution time in microseconds: %ld\n\n", execution_time);
+	printf("\t- Min value: %f\n", min_element);
+	printf("\t- Min index: %d\n", min_index);
+	printf("\t- Execution time in microseconds: %ld\n\n", execution_time);
 
 	return execution_time;
 }
 
 long int find_min_parallel(float* array, size_t size) {
-	struct timeval start, end;
-	float array_values[4], array_indexes[4];
-	
-	gettimeofday(&start, NULL);
+    struct timeval start, end;
 
-	__m128 min_elements = _mm_set1_ps(FLT_MAX);
-	__m128 increment = _mm_set1_ps(4);
-	__m128 indexes = _mm_setr_ps(0, 1, 2, 3);
-	__m128 min_indexes = _mm_setr_ps(0, 1, 2, 3);
-	__m128 value, lt;
+    __m128 min_elements = _mm_set1_ps(FLT_MAX);
+    __m128 increment = _mm_set1_ps(4);
+    __m128 indexes = _mm_setr_ps(0, 1, 2, 3);
+    __m128 min_indexes = _mm_setr_ps(0, 1, 2, 3);
+    __m128 value, lt;
 
-	for (size_t i = 0; i < size; i += 4)
-	{
-		value = _mm_loadu_ps(&array[i]);
-		lt = _mm_cmpgt_ps(min_elements, value);
-		min_indexes = _mm_blendv_ps(min_indexes, indexes, lt);
-		min_elements = _mm_min_ps(min_elements, value);
-		indexes = _mm_add_ps(indexes, increment);
-	}
+    gettimeofday(&start, NULL);
 
+    // Parallelize the loop
+    for (size_t i = 0; i < size; i += 4) {
+        // Load 4 elements from the array into a vector
+        value = _mm_loadu_ps(&array[i]);
+
+        // Compare the loaded elements to the current minimum elements
+        lt = _mm_cmpgt_ps(min_elements, value);
+
+        // Select the smaller elements and their indices
+        min_elements = _mm_blendv_ps(min_elements, value, lt);
+        min_indexes = _mm_blendv_ps(min_indexes, indexes, lt);
+
+        // Increment the indices
+        indexes = _mm_add_ps(indexes, increment);
+    }
+
+    // Store the minimum elements and indices
+    float array_values[4], array_indexes[4];
     _mm_storeu_ps(array_values, min_elements);
     _mm_storeu_ps(array_indexes, min_indexes);
 
+    // Find the minimum element and index
     float min_element = array_values[0];
     int min_index = (int) array_indexes[0];
 
-	for (size_t i = 1; i < 4; i++)
-	{
-		if (array_values[i] < min_element)
-		{
-			min_element = array_values[i];
-			min_index = (int) array_indexes[i];
-		}
-	}
+    // Check if any of the other elements are smaller
+    for (size_t i = 1; i < 4; i++) {
+        if (array_values[i] < min_element) {
+            min_element = array_values[i];
+            min_index = (int) array_indexes[i];
+        }
+    }
 
-	gettimeofday(&end, NULL);
+    // Calculate the execution time in microseconds
+    gettimeofday(&end, NULL);
+    long int execution_time = (((end.tv_sec - start.tv_sec) * 1000000) + end.tv_usec) - (start.tv_usec);
 
-	long int execution_time = (((end.tv_sec - start.tv_sec) * 1000000) + end.tv_usec) - (start.tv_usec);
-	printf("Parallel Method:\n");
-	printf("\tMin value: %f\n", min_element);
-	printf("\tMin index: %d\n", min_index);
-	printf("\tExecution time in microseconds: %ld\n\n", execution_time);
-	return execution_time;
+    // Print the results
+    printf("Parallel Method:\n");
+    printf("\t- Min value: %f\n", min_element);
+    printf("\t- Min index: %d\n", min_index);
+    printf("\t- Execution time in microseconds: %ld\n\n", execution_time);
+
+    return execution_time;
 }
 
 int main() {
 	// Show group members
     printf("Group Members:\n");
 	printf("\t- Ali Ghanbari [810199473]\n");
-	printf("\t- Behrad Elmi  [810199557]\n");
+	printf("\t- Behrad Elmi  [810199557]\n\n");
 
 	float *array = new float [ARRAY_SIZE];
 	generate_random_array(array, ARRAY_SIZE);
@@ -161,7 +174,7 @@ int main() {
 	long int serial_execution_time = find_min_serial(array, ARRAY_SIZE);
 	long int parallel_execution_time = find_min_parallel(array, ARRAY_SIZE);
 
-	printf("Speed up: %.2f\n", (static_cast <float> serial_execution_time / static_cast <float> parallel_execution_time));
+	printf("Speed up: %.2f\n", (float)serial_execution_time / (float)parallel_execution_time);
     
 	return 0;
 }
