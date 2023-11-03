2e30beae8705fc0b005d88d663454260f07ca901
BehradElmi <behradelmi1@gmail.com>
2023-11-03 22:25:44 +0330

    Add Sequential part of Q4

--- CA2/Q4/main.cpp
+++ CA2/Q4/main.cpp
@@ -17,7 +17,37 @@ int main()
 
     Mat limg = imread(logo, IMREAD_ANYCOLOR);
     Mat fimg = imread(front, IMREAD_ANYCOLOR);
+    const int HIGH_COL = limg.cols > fimg.cols ? limg.cols : fimg.cols;
+    const int HIGH_ROW = limg.rows > fimg.rows ? limg.rows : fimg.rows;
+    const int LOW_COL = limg.cols < fimg.cols ? limg.cols : fimg.cols;
+    const int LOW_ROW = limg.rows < fimg.rows ? limg.rows : fimg.rows;
     
-    
+    gettimeofday(&start, NULL);
+    for(int i = 0; i < HIGH_ROW; i++)
+    {
+        for(int j = 0; j < HIGH_COL; j++)
+        {
+            if(i > LOW_ROW || j > LOW_COL)
+            {
+                continue;
+            }
+            else
+            {
+                fimg.at<cv::Vec3b>(i, j) = 
+                    0.75 * fimg.at<cv::Vec3b>(i, j) +
+                    0.25 * limg.at<cv::Vec3b>(i, j);
+            }
+        }
+    }
+    gettimeofday(&end, NULL);
+    printf("Sequential Result:\n\t"
+           "-%ld.%ld seconds\n",
+           end.tv_sec-start.tv_sec,
+           end.tv_usec-start.tv_usec);
+
+    cv::namedWindow("Sequential", WINDOW_AUTOSIZE);
+    imshow("Sequential", fimg);
+    waitKey(0);
+
     return 0;
 }
