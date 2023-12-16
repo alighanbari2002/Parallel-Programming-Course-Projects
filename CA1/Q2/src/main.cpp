020055cfbf30ae32f4cc17516621fcf1e1280098
Ali Ghanbari <ali4.ghanbari7@gmail.com>
2023-12-16 03:45:53 -0500

    Refactor CA1 files and folder structures

--- CA1/Q2/src/main.cpp
+++ CA1/Q2/src/main.cpp
@@ -42,7 +42,7 @@ void print_u8(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=15; i>0; i--) {
+	for (size_t i = 15; i > 0; i--) {
 		printf ("%X, ", tmp.m128_u8[i]);
 	}
 	printf ("%X]\n", tmp.m128_u8[0]);
@@ -52,7 +52,7 @@ void print_i8(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=15; i>0; i--) {
+	for (size_t i = 15; i > 0; i--) {
 		printf ("%X, ", tmp.m128_i8[i]);
 	}
 	printf ("%X]\n", tmp.m128_i8[0]);
@@ -62,7 +62,7 @@ void print_u16(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=7; i>0; i--) {
+	for (size_t i = 7; i > 0; i--) {
 		printf ("%X, ", tmp.m128_u16[i]);
 	}
 	printf ("%X]\n", tmp.m128_u16[0]);
@@ -72,7 +72,7 @@ void print_i16(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=7; i>0; i--) {
+	for (size_t i = 7; i > 0; i--) {
 		printf ("%X, ", tmp.m128_i16[i]);
 	}
 	printf ("%X]\n", tmp.m128_i16[0]);
@@ -82,7 +82,7 @@ void print_u32(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=3; i>0; i--) {
+	for (size_t i = 3; i > 0; i--) {
 		printf ("%X, ", tmp.m128_u32[i]);
 	}
 	printf ("%X]\n", tmp.m128_u32[0]);
@@ -92,7 +92,7 @@ void print_i32(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=3; i>0; i--) {
+	for (size_t i = 3; i > 0; i--) {
 		printf ("%X, ", tmp.m128_i32[i]);
 	}
 	printf ("%X]\n", tmp.m128_i32[0]);
@@ -102,7 +102,7 @@ void print_u64(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=1; i>0; i--) {
+	for (size_t i = 1; i > 0; i--) {
 		printf ("%lX, ", tmp.m128_u64[i]);
 	}
 	printf ("%lX]\n", tmp.m128_u64[0]);
@@ -112,40 +112,48 @@ void print_i64(__m128i a) {
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
-	for (int i=1; i>0; i--) {
+	for (size_t i = 1; i > 0; i--) {
 		printf ("%lX, ", tmp.m128_i64[i]);
 	}
-	printf ("%lX]\n\n", tmp.m128_i64[0]);
+	printf ("%lX]\n", tmp.m128_i64[0]);
 }
 
 void print_int_vector(__m128i a, DataType type) {
     switch (type) {
         case u8:
-            print_u8(a); // print 16 unsigned bytes
+    		printf("\t- 16 unsigned bytes format: ");
+            print_u8(a);
             break;
         case i8:
-            print_i8(a); // print 16 signed bytes
+    		printf("\t- 16 signed bytes format: ");
+            print_i8(a);
             break;
         case u16:
-            print_u16(a); // print 8 unsigned words
+    		printf("\t- 8 unsigned words format: ");
+            print_u16(a);
             break;
         case i16:
-            print_i16(a); // print 8 signed words
+    		printf("\t- 8 signed words format: ");
+            print_i16(a);
             break;
         case u32:
-            print_u32(a); // print 4 unsigned double words
+    		printf("\t- 4 unsigned double words format: ");
+            print_u32(a);
             break;
         case i32:
-            print_i32(a); // print 4 signed double words
+    		printf("\t- 4 signed double words format: ");
+            print_i32(a);
             break;
         case u64:
-            print_u64(a); // print 2 unsigned quad words
+    		printf("\t- 2 unsigned quad words format: ");
+            print_u64(a);
             break;
         case i64:
-            print_i64(a); // print 2 signed quad words
+    		printf("\t- 2 signed quad words format: ");
+            print_i64(a);
             break;
         default:
-            printf("Invalid type.\n");
+            printf("Invalid type!\n");
             break;
     }
 }
@@ -154,41 +162,41 @@ void print_spfp_vector(__m128 a) {
 	floatVec tmp;
 	tmp.f128 = a;
 	printf ("[");
-	for (int i=3; i>0; i--) {
+	for (size_t i = 3; i > 0; i--) {
 		printf ("%f, ", tmp.f32[i]);
 	}
 	printf ("%f]\n", tmp.f32[0]);
 }
 
+void print_group_info() {
+    printf("Group Members:\n");
+	printf("\t- Ali Ghanbari [810199473]\n");
+	printf("\t- Behrad Elmi  [810199557]\n");
+}
+
 int main() {
+	print_group_info();
+
     __m128i a;
 	unsigned char intArray [16] = {	0X00, 0X11, 0X22, 0X33, 0X44, 0X55, 0X66, 0X77,
 					    			0X88, 0X99, 0XAA, 0XBB, 0XCC, 0XDD, 0XEE, 0XFF };
 	a = _mm_load_si128((const __m128i*)intArray);
 
-    printf("Testing print_int_vector Function:\n");
-    printf("\t- 16 unsigned bytes format: ");
+    printf("\nTesting \"print_int_vector\" Function:\n");
 	print_int_vector(a, u8);
-    printf("\t- 16 signed bytes format: ");
 	print_int_vector(a, i8);
-    printf("\t- 8 unsigned words format: ");
 	print_int_vector(a, u16);
-    printf("\t- 8 signed words format: ");
 	print_int_vector(a, i16);
-    printf("\t- 4 unsigned double words format: ");
 	print_int_vector(a, u32);
-    printf("\t- 4 signed double words format: ");
 	print_int_vector(a, i32);
-    printf("\t- 2 unsigned quad words format: ");
 	print_int_vector(a, u64);
-    printf("\t- 2 signed quad words format: ");
 	print_int_vector(a, i64);
 
 	__m128 b;
 	float floatArray[4] = {-6763.4, -3.1f, 1231231232.5, 7.0f};
 	b = _mm_load_ps(floatArray);
 
-    printf("Testing print_spfp_vector Function:\n");
+    printf("\nTesting \"print_spfp_vector\" Function:\n");
     printf("\t- Content of the 128-bit register: ");
 	print_spfp_vector(b);
 
