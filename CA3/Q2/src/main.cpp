bd1e42bbed564f75db0e1894f1f38511fd690795
Ali Ghanbari <ali4.ghanbari7@gmail.com>
2023-12-17 07:16:44 -0500

    Refactor code to improve performance

--- CA3/Q2/src/main.cpp
+++ CA3/Q2/src/main.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <iostream>
+#include <random>
 #include <stdlib.h>
-#include <float.h>
+#include <time.h>
 #include <math.h>
-#include <chrono>
 #include <omp.h>
 #ifdef 		_WIN32
 #include <intrin.h>
@@ -10,46 +10,42 @@
 #include <x86intrin.h>
 #endif
 
-using std::chrono::high_resolution_clock;
-using std::chrono::duration_cast;
-using std::chrono::nanoseconds;
-
 #define ARRAY_SIZE 1048576 // 2 ^ 20
-#define NUM_THREADS 8
+#define NUM_THREADS omp_get_max_threads()
 
-void generate_random_array(double* arr, size_t size) {
-	double min = 0;
-	double max = pow(10, 6);
-	double range = max - min;
-	
-	for (size_t i = 0; i < size; i++) {
-		srand(time(NULL));
-		float random =  ((double)rand()) / (double)RAND_MAX;
-		arr[i] = random * range + min;
-	}
+using std::default_random_engine; 
+using std::uniform_real_distribution;
+
+void generate_random_array(double*& array, const size_t& size) {
+    default_random_engine generator(time(NULL));
+    uniform_real_distribution<double> distribution(0.0, pow(10, 6));
+    
+	for (size_t i = 0; i < size; ++i) {
+        array[i] = distribution(generator);
+    }
 }
 
-double find_average_and_std_serial(double* array, size_t size) {
+double find_average_and_std_serial(double*& array, const size_t& size) {
 	double sum = 0, average;
 	double sum_squared_diff = 0, difference, standard_deviation;
 	size_t i;
 
-	auto start = high_resolution_clock::now();
+	double start = omp_get_wtime();
 
 	// Average
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; ++i)
 		sum += array[i];
 	average = sum / size;
 
 	// Standard Deviation
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < size; ++i) {
 		difference = array[i] - average;
 		sum_squared_diff += difference * difference;
 	}
 	standard_deviation = sqrt(sum_squared_diff / size);
 
-	auto finish = high_resolution_clock::now();
-	double execution_time = duration_cast<nanoseconds>(finish - start).count();
+	double finish = omp_get_wtime();
+	double execution_time = (finish - start) * pow(10, 9);
 
 	printf("\nSerial Method:\n");
 	printf("\t- Average: %.4lf\n", average);
@@ -59,29 +55,31 @@ double find_average_and_std_serial(double* array, size_t size) {
 	return execution_time;
 }
 
-double find_average_and_std_parallel(double* array, size_t size) {
+double find_average_and_std_parallel(double*& array, const size_t& size) {
 	double sum = 0, average;
 	double sum_squared_diff = 0, difference, standard_deviation;
 	size_t i;
 
-	auto start = high_resolution_clock::now();
+	double start = omp_get_wtime();
 
 	// Average
-	#pragma omp parallel for simd num_threads(NUM_THREADS) shared(array) private(i) reduction(+:sum)
-		for (i = 0; i < size; i++)
+	#pragma omp parallel for simd default(shared) private(i) reduction(+:sum) \
+			schedule(auto) num_threads(NUM_THREADS)
+		for (i = 0; i < size; ++i)
 			sum += array[i];
 	average = sum / size;
 
 	// Standard Deviation
-	#pragma omp parallel for simd num_threads(NUM_THREADS) shared(array, average) private(difference, i) reduction(+:sum_squared_diff)
-		for (i = 0; i < size; i++) {
+	#pragma omp parallel for simd default(shared) private(difference, i) \
+			reduction(+:sum_squared_diff) schedule(auto) num_threads(NUM_THREADS)
+		for (i = 0; i < size; ++i) {
 			difference = array[i] - average;
 			sum_squared_diff += difference * difference;
 		}
 	standard_deviation = sqrt(sum_squared_diff / size);
 
-	auto finish = high_resolution_clock::now();
-	double execution_time = duration_cast<nanoseconds>(finish - start).count();
+	double finish = omp_get_wtime();
+	double execution_time = (finish - start) * pow(10, 9);
 
 	printf("\nParallel Method:\n");
 	printf("\t- Average: %.4lf\n", average);
