9144ec10be31d0f9acfdbc8f125909a5f51861ca
Ali Ghanbari <ali4.ghanbari7@gmail.com>
2024-01-12 15:10:59 -0500

    Solve Q4

--- CA4/Q4/src/main.cpp
+++ CA4/Q4/src/main.cpp
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <type_traits>
-#include <ctime>
-#include <chrono>
 #include <unistd.h>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#ifdef 		_WIN32
-#include <intrin.h>
-#else
-#include <x86intrin.h>
-#endif
+#include <chrono>
+#include <pthread.h>
 
 using cv::Mat;
 using cv::imwrite;
 using cv::imread;
 using cv::IMREAD_GRAYSCALE;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::nanoseconds;
+
+typedef struct {
+    size_t row;
+    Mat out_img;
+} ThreadData;
 
 #define FRONT_IAMGE "../assets/front.png"
 #define LOGO_IMAGE  "../assets/logo.png"
 #define OUTPUT_DIR  "../output/"
 #define ALPHA 0.25
-#define M128_GRAY_INTERVAL 16
 #define NUM_THREADS static_cast<size_t>(sysconf(_SC_NPROCESSORS_ONLN) - 1)
-#define MIL 1000000
 
 // Global variables
 Mat logo;
@@ -34,37 +34,32 @@ unsigned int LOGO_COL;
 unsigned int FRONT_ROW;
 unsigned int FRONT_COL;
 
-// Thread data struct
-typedef struct{
-    size_t row_begin;
-    size_t row_end;
-    size_t col_begin;
-    size_t col_end;
-    Mat* out_img;
-
-} ThreadData;
-
-using std::chrono::high_resolution_clock;
-using std::chrono::duration_cast;
-using std::chrono::nanoseconds;
-
-
-double serial_implementation() {
+double serial_implementation()
+{
     Mat out_img_serial(FRONT_ROW, FRONT_COL, CV_8U);
     size_t row, col;
     
     auto start = high_resolution_clock::now();
 
-    for(row = 0; row < FRONT_ROW; ++row) {
-        for(col = 0; col < FRONT_COL; ++col) {
-            if(row <= LOGO_ROW && col <= LOGO_COL) {
+    for(row = 0; row < FRONT_ROW; ++row)
+    {
+        for(col = 0; col < FRONT_COL; ++col)
+        {
+            if(row <= LOGO_ROW && col <= LOGO_COL)
+            {
                 if(front.at<uchar> (row, col) + ALPHA * logo.at<uchar> (row, col) > 255)
+                {
                     out_img_serial.at<uchar> (row, col) = 255;
+                }
                 else
+                {
                     out_img_serial.at<uchar> (row, col) = front.at<uchar> (row, col) + ALPHA * logo.at<uchar> (row, col);
+                }
             }
             else
+            {
                 out_img_serial.at<uchar> (row, col) = front.at<uchar> (row, col);
+            }
         }
     }
 
@@ -79,63 +74,105 @@ double serial_implementation() {
     return execution_time;
 }
 
-void* partial_blend_image(void* arg){
-    
+void* process_image(void* arg)
+{
     ThreadData* data = (ThreadData*)arg;
+    size_t start_row = data->row * (FRONT_ROW / NUM_THREADS);
+    size_t end_row = (data->row + 1) * (FRONT_ROW / NUM_THREADS);
 
-    for(size_t i = data->row_begin; i < data->row_end; i++){
-        for(size_t j = data->col_begin; j < data->col_end; i++){
-            data->out_img = 
+    for (size_t row = start_row; row < end_row; ++row)
+    {
+        for (size_t col = 0; col < FRONT_COL; ++col)
+        {
+            if(row <= LOGO_ROW && col <= LOGO_COL)
+            {
+                if(front.at<uchar> (row, col) + ALPHA * logo.at<uchar> (row, col) > 255)
+                {
+                    data->out_img.at<uchar> (row, col) = 255;
+                }
+                else
+                {
+                    data->out_img.at<uchar> (row, col) = front.at<uchar> (row, col) + ALPHA * logo.at<uchar> (row, col);
+                }
+            }
+            else
+            {
+                data->out_img.at<uchar> (row, col) = front.at<uchar> (row, col);
+            }
         }
     }
+
+    pthread_exit(NULL);
 }
 
-double parallel_implementation() {
+double parallel_implementation()
+{
     Mat out_img_parallel(FRONT_ROW, FRONT_COL, CV_8U);
-    size_t row, col;
+    size_t i;
+
+    pthread_t threads[NUM_THREADS];
+    ThreadData thread_data_array[NUM_THREADS];
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
     auto start = high_resolution_clock::now();
 
-    #pragma omp parallel for simd default(shared) private(row, col) schedule(guided, 16) num_threads(NUM_THREADS) simdlen(16)
-        for(row = 0; row < FRONT_ROW; ++row) {
-            for(col = 0; col < FRONT_COL; ++col) {
-                if(row <= LOGO_ROW && col <= LOGO_COL) {
-                    if(front.at<uchar> (row, col) + ALPHA * logo.at<uchar> (row, col) > 255)
-                        out_img_parallel.at<uchar> (row, col) = 255;
-                    else
-                        out_img_parallel.at<uchar> (row, col) = front.at<uchar> (row, col) + ALPHA * logo.at<uchar> (row, col);
-                }
-                else
-                    out_img_parallel.at<uchar> (row, col) = front.at<uchar> (row, col);
-            }
+    // Assign the arguments for each thread
+    for (i = 0; i < NUM_THREADS; ++i)
+    {
+        thread_data_array[i].row = i;
+        thread_data_array[i].out_img = out_img_parallel;
+
+        int rc = pthread_create(&threads[i], &attr, process_image, &thread_data_array[i]);
+        if (rc)
+        {
+            printf("ERROR: return code from pthread_create() is %d\n", rc);
+            exit(-1);
         }
+    }
+
+    // Free the attribute and join the threads
+    pthread_attr_destroy(&attr);
+    for (i = 0; i < NUM_THREADS; ++i)
+    {
+        int rc = pthread_join(threads[i], NULL);
+        if (rc)
+        {
+            printf("ERROR: return code from pthread_join() is %d\n", rc);
+            exit(-1);
+        }
+    }
 
 	auto finish = high_resolution_clock::now();
 	double execution_time = duration_cast<nanoseconds>(finish - start).count();
 
-    
     imwrite(OUTPUT_DIR "parallel output.png", out_img_parallel);
     out_img_parallel.release();
 
     printf("\t- Parallel Method: %.4lf\n", execution_time);
-
+    
     return execution_time;
 }
 
-void print_group_info() {
+void print_group_info()
+{
     printf("Group Members:\n");
 	printf("\t- Ali Ghanbari [810199473]\n");
 	printf("\t- Behrad Elmi  [810199557]\n");
 }
 
-int main() {
+int main()
+{
 	print_group_info();
 
     // Load frames
     logo  = imread(LOGO_IMAGE, IMREAD_GRAYSCALE);
     front = imread(FRONT_IAMGE, IMREAD_GRAYSCALE);
     if (logo.size().width > front.size().width ||
-        logo.size().height > front.size().height) {
+        logo.size().height > front.size().height)
+    {
         printf("Illegal frames!\n");
         exit(EXIT_FAILURE);
     }
