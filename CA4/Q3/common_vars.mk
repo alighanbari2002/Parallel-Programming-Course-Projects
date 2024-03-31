939ee441bb947f734f73ad9a194e19abef317b25
Ali Ghanbari <ali4.ghanbari7@gmail.com>
2024-03-30 21:11:39 -0400

    feat: enhance Makefile and common_vars.mk
    1. Refactor run and report targets in Makefile to use a shared command.
    2. Add new runlog and applylog targets to Makefile for logging.
    3. Add pedantic flag to CXXFLAGS in common_vars.mk.

--- CA4/Q3/common_vars.mk
+++ CA4/Q3/common_vars.mk
@@ -1,12 +1,16 @@
 CXX      = g++
-CXXFLAGS += -Wall -Wno-multichar -pthread
+CXXFLAGS += -Wall -pedantic -pthread
 CXX      += $(CXXFLAGS)
 CPPFLAGS += -std=c++11
 
+AR      = ar
+ARFLAGS += rvs
+
 RM     = rm -f
 RMDIR  = rm -rf
-MKDIR  = @mkdir -p
-COPY   = @cp
-MOVE   = @mv -f
-RENAME = @mv -f
+MKDIR  = mkdir -p
+COPY   = cp
+MOVE   = mv -f
+RENAME = mv -f
+
 NULL_DEVICE = /dev/null
